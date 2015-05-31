738f7e4ad0793e1576373db411cda4964e662b37
Doug Swisher <big.swish@gmail.com>
2015-05-31 17:06:40 -0500

    Add (non-functional) sync command

---
+++ src/Kurdle/Commands/SyncCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Yaclops;
+using Yaclops.Attributes;
+
+namespace Kurdle.Commands
+{
+    [Summary("Take the most recently generated site and sync it with the cloud.")]
+    public class SyncCommand : ISubCommand
+    {
+        public void Execute()
+        {
+            // TODO
+            Console.WriteLine("Sync is not yet implemented.");
+        }
+    }
+}
