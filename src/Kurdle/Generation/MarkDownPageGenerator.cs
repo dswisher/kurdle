6c40594fcd47bc074a2c5e50d3cdaf30c23fb8c5
Doug Swisher <big.swish@gmail.com>
2015-06-01 08:08:38 -0500

    Checkpoint

--- src/Kurdle/Generation/MarkDownPageGenerator.cs
+++ src/Kurdle/Generation/MarkDownPageGenerator.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Xml;
 using CommonMark;
 using RazorEngine.Templating;
 
@@ -15,7 +17,50 @@ namespace Kurdle.Generation
 
         protected override string GetPageContent(TextReader reader)
         {
-            return CommonMarkConverter.Convert(reader.ReadToEnd());
+            // Process markdown to HTML...
+            var rawContent = CommonMarkConverter.Convert(reader.ReadToEnd());
+
+            // Post-process the HTML...
+            XmlDocument xml = new XmlDocument();
+
+            try
+            {
+                xml.LoadXml("<body>" + rawContent + "</body>");
+            }
+            catch (Exception)
+            {
+                const string name = "invalid-html.xml";
+
+                Console.WriteLine("Raw content is invalid XML.");
+                DumpContent(rawContent, name);
+                throw;
+            }
+
+            // TODO - post processing
+
+            var content = xml.SelectSingleNode("body").InnerXml;
+
+            // Return what we've got...
+            return content;
+        }
+
+
+
+        private void DumpContent(string content, string name)
+        {
+            try
+            {
+                string path = Path.Combine(_projectInfo.OutputDirectory.FullName, name);
+
+                File.WriteAllText(path, content);
+
+                Console.WriteLine("Invalid content written to {0}.", path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unhandled exception attempting to dump invalid XML.");
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
